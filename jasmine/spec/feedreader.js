1461c1b92fface37825b0a9a8ac46236b98974a3
Bruno Li <lisiuloong@gmail.com>
2018-06-26 19:03:52 -0400

    add test to check if there a feed entry

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -88,13 +88,15 @@ $(
           expect(menuClick.classList.contains("menu-hidden")).toBe(true);
 
         } else if (!menuClick) {
-            
+
           expect(menuClick.classList.contains("menu-hidden")).toBe(false);
         }
       });
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe("Initial Entries", function() {
+
 
     /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -103,11 +105,39 @@ $(
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+         // before each for asynchronous call
+         beforeEach(function(done){
+             loadFeed(0,done);
+         });
+
+         it("should have at least a single entry",function(){
+             // check if element has at least one entry by checking the length property from the DOM.
+            let entries = document.querySelector(".feed").getElementsByClassName("entry");
+            expect(entries.length).toBeGreaterThan(0);
+         });
+
+    });
+
+
+      /* TODO: Write a new test suite named "New Feed Selection" */
+      describe("New Feed Selection", function() {
 
     /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
+         * 
          */
+
+
+
+
+
+
+      });
+
+
+
+
+
   })()
 );
